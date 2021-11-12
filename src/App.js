48e79e3a008890016757fdf75da5e24d4a4fb9df
sadiksha1000 <60103456+sadiksha1000@users.noreply.github.com>
2021-11-12 16:53:35 +0545

    Props & States

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+// Root Component
 
-function App() {
+import './App.css'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import {useState} from 'react'
+
+const App=()=> {
+  const[tasks,setTasks]=useState([
+    {
+      id:1,
+      text:'Doctors Appointment',
+      day:'4th September at 2:30 P.M',
+      reminder:true
+    },
+    {
+      id:2,
+      text:'Meeting at School',
+      day:'6th September at 9:30 A.M',
+      reminder:false
+    },
+    {
+      id:3,
+      text:'Grocery Shopping',
+      day:'6th September at 5:30 P.M',
+      reminder:true
+    }
+  ])
+  // const name='Sadiksha'
+  // const x=true
+
+  // Delete Task
+  const deleteTask = (id) =>{
+    setTasks(tasks.filter((task)=>task.id !==id))
+  }
+
+  // Toggle Reminder
+  const toggleReminder =(id)=>{
+    // setTasks(tasks.map((task)=>task.id===id ? {...task,reminder:!task.reminder} : task))
+    console.log('Toggled')
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header title='Task Tracker'/>
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />):('No Tasks Left. Hurrray!!!')}
+      
     </div>
   );
 }
 
+// Class based component
+
+// class App extends React.Component{
+//   render(){
+//     return
+//       <h1>Hello from class</h1>
+//   }
+// }
 export default App;
