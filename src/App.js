7938eba8a94f1d2d9e618bc8ab93ca333c17a0dd
sadiksha1000 <60103456+sadiksha1000@users.noreply.github.com>
2021-11-13 16:55:21 +0545

    Toggle & Component State

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import {useState} from 'react'
+import AddTask from './components/AddTask'
 
 const App=()=> {
+  const [showAddTask,setShowAddTask]=useState(false)
   const[tasks,setTasks]=useState([
     {
       id:1,
@@ -29,6 +31,13 @@ const App=()=> {
   // const name='Sadiksha'
   // const x=true
 
+  // Add Task
+  const addTask = (task)=>{
+    const id=Math.floor(Math.random()*1000)+1
+    const newTask={id,...task}
+    setTasks([...tasks,newTask])
+  }
+
   // Delete Task
   const deleteTask = (id) =>{
     setTasks(tasks.filter((task)=>task.id !==id))
@@ -42,7 +51,8 @@ const App=()=> {
   
   return (
     <div className="container">
-      <Header title='Task Tracker'/>
+      <Header onAdd={()=>setShowAddTask(!showAddTask)} showAdd={showAddTask} title='Task Tracker'/>
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />):('No Tasks Left. Hurrray!!!')}
       
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({title}) => {
-  const onClick=()=>{
-    console.log('Clicked')
-  }
+const Header = ({title,onAdd,showAdd}) => {
+  // const onAdd=()=>{
+  //   console.log('Clicked')
+  // }
   
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button color='green' text='Add' onClick={onClick} />
+      <Button color={showAdd ? 'red':'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
     </header>
   )
 }
